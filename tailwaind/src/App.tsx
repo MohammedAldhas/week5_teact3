454a048cb621fecaee2c2da591fbce1fc141049e
Mohammed <mohammed.aldhas3@gmail.com>
2023-08-23 14:21:13 +0300

    f

--- tailwaind/src/App.tsx
+++ tailwaind/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Aboute from "./pages/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 // import Contact from "./pages/Contact";
 import Error from "./pages/Error";
 // import Card from "./components/Card";
@@ -19,22 +19,14 @@ function App() {
           <Route path="" element={<Contact></Contact>}></Route>
           <Route path="info" element={<Info></Info>}></Route>
         </Route> */}
-        
+
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/about" element={<Aboute></Aboute>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="*" element={<Error></Error>}></Route>
       </Routes>
-      
     </>
   );
 }
 
 export default App;
-
-
-// أنشاء مشروع جديد باسم react-02-lab . -داخل ملف src أنشاء مجلد components , بداخله انشاء مكوّن باستخدام طريقة Function حيت يكون مسؤول navbar وcomponents اخر اعرض بداخله معلومات المعسكر و مكون اخر يعرض صفحة التسجيل .
-// قم بأستدعاء جميع المكونات وعرضها في الصفحة الرئسية .
-// إجعل مكون navbar ظاهرا في جميع الصفحات , و صفحه محدده لعرض بيانات كل صفحة على حده .
-// ضع عنوان مخصص (URL) لكل صفحة .
-// ضع خيارات التنقل بين صفحات المشروع في الـnavbar .
